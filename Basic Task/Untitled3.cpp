921526a31bd455bf3ef13a24f00bdb29d1e22c12
Ayesha <aishaaisha1713@gmail.com>
2025-02-12 22:26:02 +0500

    Add files via upload

---
+++ Basic Task/Untitled3.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	string name="Ayesha";
+	int sapid=62853;
+	string course="CS";
+	int sem=1;
+	cout<<"NAME: "<<name;
+	cout<<"\nSAPID: "<<sapid;
+	cout<<"\nCOURSE: "<<course;
+	cout<<"\nSEMESTER: "<<sem;
+	
+	return 0;
+	
+}
